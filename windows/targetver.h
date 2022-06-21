90e51d1529a82d94e4a8153e2acd58665c8ac659
Raul Metsma <raul@metsma.ee>
2022-06-21 13:35:05 +0300

    Allow to install extension without x86 binary
    IB-7443
    
    Signed-off-by: Raul Metsma <raul@metsma.ee>

--- windows/targetver.h
+++ windows/targetver.h
@@ -6,18 +6,18 @@
 // including the version specified.
 
 #ifndef WINVER
-#define WINVER 0x0601
+#define WINVER 0x0A00
 #endif
 
 #ifndef _WIN32_WINNT
-#define _WIN32_WINNT 0x0601
+#define _WIN32_WINNT 0x0A00
 #endif
 
 #ifndef _WIN32_WINDOWS
-#define _WIN32_WINDOWS 0x0601
+#define _WIN32_WINDOWS 0x0A00
 #endif
 
 #ifndef _WIN32_IE
-#define _WIN32_IE 0x0601
+#define _WIN32_IE 0x0A00
 #endif
 
